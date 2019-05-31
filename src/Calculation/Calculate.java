e793b561c63ab596895cb2719fcfe23cec38a766
Javonet <support@javonet.com>
2019-05-31 12:05:56 +0000

    init

---
+++ src/Calculation/Calculate.java
@@ -0,0 +1,75 @@
+package Calculation;
+
+import Common.Activation;
+import static Common.JavonetHelper.Convert;
+import static Common.JavonetHelper.getGetObjectName;
+import static Common.JavonetHelper.getReturnObjectName;
+import static Common.JavonetHelper.ConvertToConcreteInterfaceImplementation;
+import Common.JavonetHelper;
+import Common.MethodTypeAnnotation;
+import com.javonet.Javonet;
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NObject;
+import com.javonet.api.NEnum;
+import com.javonet.api.keywords.NRef;
+import com.javonet.api.keywords.NOut;
+import com.javonet.api.NControlContainer;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
+import java.lang.*;
+import Calculation.*;
+
+public class Calculate {
+  protected NObject javonetHandle;
+
+  public Calculate() {
+    try {
+      javonetHandle = Javonet.New("Calculation.Calculate");
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+    }
+  }
+
+  public Calculate(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  public void setJavonetHandle(NObject handle) {
+    this.javonetHandle = handle;
+  }
+  /** Method */
+
+  @MethodTypeAnnotation(type = "Method")
+  public java.lang.Integer Add(java.lang.Integer a, java.lang.Integer b) {
+    try {
+      java.lang.Integer res = javonetHandle.invoke("Add", a, b);
+      if (res == null) return 0;
+      return res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return 0;
+    }
+  }
+  /** Method */
+
+  @MethodTypeAnnotation(type = "Method")
+  public java.lang.Integer Sub(java.lang.Integer a, java.lang.Integer b) {
+    try {
+      java.lang.Integer res = javonetHandle.invoke("Sub", a, b);
+      if (res == null) return 0;
+      return res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return 0;
+    }
+  }
+
+  static {
+    try {
+      Activation.initializeJavonet();
+    } catch (java.lang.Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
